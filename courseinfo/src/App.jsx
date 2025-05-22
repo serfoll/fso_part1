51a801b69566ba17a036de93131a1d6d2d57bf59
serfoll <sergiofollyvi@gmail.com>
2025-05-22 16:41:10 +0200

    Exercise 1.5 single Object for course name and parts

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,10 +1,12 @@
 /** @format */
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  console.log(`props in Header: ${JSON.stringify(props)}`);
+  return <h1>{props.name}</h1>;
 };
 
 const Part = (props) => {
+  console.log(`props in Part: ${JSON.stringify(props)}`);
   const part = props.part;
   return (
     <p>
@@ -36,27 +38,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
